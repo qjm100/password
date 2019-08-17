a2c1fed8755ad642a86194910e4fa2e591de92f2
qjm100 <53941012+qjm100@users.noreply.github.com>
2019-08-17 13:37:45 +0800

    Add files via upload

---
+++ veiji1.1.py
@@ -0,0 +1,102 @@
+#_*_coding:utf-8_*_
+import getopt
+from string import ascii_lowercase as lowercase
+import sys
+try:
+    import pyfiglet
+except ImportError:
+    print ("请安装pyfiglet 请运行 pip install pyfiglet")
+ke = None
+def En(p, key):
+    p = get_trim_text(p)
+    ptLen = len(p)
+    keyLen = len(key)
+
+    quotient = ptLen // keyLen  # 商
+    remainder = ptLen % keyLen  # 余
+
+    out = ""
+    for i in range(0, quotient):
+        for j in range(0, keyLen):
+            c = int((ord(p[i * keyLen + j]) - ord('a') + ord(key[j]) - ord('a')) % 26 + ord('a'))
+            
+            out += chr(c)
+
+    for i in range(0, remainder):
+        c = int((ord(p[quotient * keyLen + i]) - ord('a') + ord(key[i]) - ord('a')) % 26 + ord('a'))
+        out += chr(c)
+
+    return out
+
+def De(output, key):
+    ptLen = len(output)
+    keyLen = len(key)
+
+    quotient = ptLen // keyLen
+    remainder = ptLen % keyLen
+
+    inp = ""
+
+    for i in range(0, quotient):
+        for j in range(0, keyLen):
+            c = int((ord(output[i * keyLen + j]) - ord('a') - (ord(key[j]) - ord('a'))) % 26 + ord('a'))
+            # global input
+            inp += chr(c)
+
+    for i in range(0, remainder):
+        c = int((ord(output[quotient * keyLen + i]) - ord('a') - (ord(key[i]) - ord('a'))) % 26 + ord('a'))
+        # global input
+        inp += chr(c)
+        return inp
+def helpyou ():
+    print ("欢迎来到维吉尼亚\n-h --help 显示帮助\n-k --key 必要参数，秘钥\n-e --jiami输入加密信息\n-d --jiemi 输入要解密信息")
+
+def get_trim_text(text):
+    text = text.lower()
+    trim_text = ''
+    for l in text:
+        if lowercase.find(l) >= 0:
+            trim_text += l
+    return trim_text 
+
+if __name__ == '__main__':
+    
+    
+    a = pyfiglet.Figlet (font='slant')
+    print (a.renderText ('virginia'))
+
+    try:
+
+        longargs = ['help=','jiami=','jiemi=','key=']
+        opts,args= getopt.getopt( sys.argv[1:], 'he:d:k:', longargs)
+
+        b = True    
+        for o, a in opts:
+            if o in ("--key","-k"):
+                ke = a
+                
+            if o in ("--help" ,"-h"):
+                helpyou()
+                sys.exit()
+            if o in ("--jiami" ,"-e"):
+                if ke == None:
+                    print ("无秘钥，请确保输入秘钥！")
+                    break
+                jiami = a
+                print (En (jiami,ke))
+                b = False
+            if o in ("--jiemi" ,"-d"):
+                if ke == None:
+                    print ("无秘钥，请确保输入秘钥！")
+                    break
+
+                jiemi = a
+                print (De (jiemi,ke))
+                b = False
+        
+            
+    except getopt.GetoptError:
+        helpyou ()
+
+    if b == True:
+        helpyou ()
